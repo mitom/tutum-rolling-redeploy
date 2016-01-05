d2bb91e35bc2f4342666e53429eb07b623e5d80f
Tamás Millián <tamas.millian@gmail.com>
2016-01-05 02:54:37 +0100

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# Simple rolling deploy for tutum
+
+**Disclaimer:** Use this at your own risk, any issues or harm caused by this script is your own responsibility.
+
+The aim is to get 0 downtime updates.
+
+It will go through all *running* and *starting* (at the time when the script is ran) containers of a service and redeploy them one by one.
+The next container will be issued a redeploy once a *new* container comes up, meaning if one was *starting* when the script was run
+ and came up, it will just wait for its' turn and be redeployed, since it is possible it was crashing. If a container that
+ wasn't in the list comes up, it will trigger the redeploy for the next one in line, because we have 1 extra one running now, so even if
+ the previous one we issued the redeploy to isn't finished yet, we should be safe.
+
+I'd recommend using this on a service that is behind a load balancer that watches for tutum container states rather than just reachability.
+Check out my [nginx load balancer for tutum](https://hub.docker.com/r/mitom/tutum-nginx-loadbalancer/) or use HAproxy if you don't want to bother with nginx.
+
+## Requirements
+
+The only requirements are python and `tutum-python` (get it with pip).
+
+## Usage
+
+The script runs on env variables:
+`TUTUM_USER` - the username in tutum
+`TUTUM_APIKEY` - an api key for tutum
+`TUTUM_AUTH` - the auth header for tutum, handy if this runs in a docker container in tutum with full access
+`TUTUM_SERVICE` - the uuid of the service to redeploy
+`TUTUM_GRACE_PERIOD` - a number in seconds to wait before issuing the next redeploy after a container comes up. Defaults to 0
+
+Either `TUTUM_USER` AND `TUTUM_APIKEY` are needed OR `TUTUM_AUTH`, but `TUTUM_AUTH` will be preferred if all are present.
---
+++ redeploy.py
@@ -0,0 +1,91 @@
+import tutum
+import websocket
+import base64
+import os
+import sys
+import json
+import time
+
+service_uuid = os.environ.get('TUTUM_SERVICE')
+username = os.environ.get('TUTUM_USER')
+apikey = os.environ.get('TUTUM_APIKEY')
+tutum_auth = os.environ.get('TUTUM_AUTH')
+grace_period = os.environ.get('TUTUM_GRACE_PERIOD') or 0
+
+if (not (username and apikey) and not tutum_auth):
+    raise EnvironmentError('You should either give full access to this service, or provide TUTUM_USER and TUTUM_APIKEY as env variables')
+if (not service_uuid):
+    raise EnvironmentError('You must set the TUTUM_SERVICE env var.')
+
+service_full = '/api/v1/service/'+service_uuid+'/'
+nginx_config_dir = '/etc/nginx/conf.d/'
+
+errors = []
+containers = []
+
+def redeploy_next():
+    if (len(containers) == 0):
+        if len(errors):
+            sys.exit(str(len(errors)) + " errors have happened.")
+
+        print "All is well."
+        sys.exit()
+
+    container = containers.pop()
+    print "Redeploying " + container.name
+    try:
+        container.redeploy()
+    except Exception as error:
+        print "Unexpected error:" + str(error)
+        print "Trying to continue"
+        errors.append(error)
+        redeploy_next()
+
+
+def is_new_container(container):
+    for c in containers:
+        if c.resource_uri == container['resource_uri']:
+            return False
+    return True
+
+def on_message(ws, message):
+    message = json.loads(message)
+    if message['type'] == 'container' and message['state'] == 'Running':
+        if (is_new_container(message)):
+            time.sleep(grace_period)
+            redeploy_next()
+
+def on_error(ws, error):
+    sys.exit(error)
+
+def on_close(ws):
+    print "Stream closed."
+
+def on_open(ws):
+    print "Stream opened."
+    redeploy_next()
+
+if (tutum_auth):
+    header = "Authorization: " + tutum_auth
+else:
+    header = "Authorization: Basic %s" % base64.b64encode("%s:%s" % (username, apikey))
+
+
+ws = websocket.WebSocketApp('wss://stream.tutum.co/v1/events',
+                            header=[header],
+                            on_message=on_message,
+                            on_error=on_error,
+                            on_close=on_close,
+                            on_open=on_open
+)
+
+existing_containers = tutum.Container.list(service=service_full)
+for container in existing_containers:
+    if container.state in ['Running', 'Starting']:
+        print vars(container)
+        containers.append(container)
+
+print "Found " + str(len(containers)) + " containers"
+
+
+ws.run_forever()
